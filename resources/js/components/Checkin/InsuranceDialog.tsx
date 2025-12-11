d2396127b0df590fe340113ab694e84daeec2ee3
justdick <dickson.owusu@gmail.com>
2025-12-11 22:39:45 +0000

    fix: Make InsuranceDialog scrollable for smaller screens

--- resources/js/components/Checkin/InsuranceDialog.tsx
+++ resources/js/components/Checkin/InsuranceDialog.tsx
@@ -151,7 +151,7 @@ export default function InsuranceDialog({
 
     return (
         <Dialog open={open} onOpenChange={handleModalClose}>
-            <DialogContent className="max-w-2xl">
+            <DialogContent className="max-w-2xl max-h-[90vh] overflow-y-auto">
                 <DialogHeader>
                     <DialogTitle className="flex items-center gap-2">
                         <Shield className="h-5 w-5 text-primary" />
